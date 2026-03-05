618a9578c3336345d871ee1379e8b3a3346752d0
NabdipDEV <nabdipdevsharma@gmail.com>
2026-03-05 17:39:04 +0530

    Fix lesson buttons & word display issues

--- script/index.js
+++ script/index.js
@@ -4,13 +4,23 @@ const loadLessons = () => {
         .then((res) => res.json())
         .then((json) => displayLesson(json.data));
 }
+// step-5
+const removeActive = () => {
+    const lessonbtons = document.querySelectorAll(".lesson-btn")
+    lessonbtons.forEach((btn) => btn.classList.remove("active"))
+}
 
 // step-3
 const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
         .then((res) => res.json())
-        .then((card) => wordDisplay(card.data));
+        .then((card) => {
+            removeActive()
+            const clickBtn = document.getElementById(`lesson-btn-${id}`)
+            clickBtn.classList.add("active")
+            wordDisplay(card.data)
+        });
 }
 
 
@@ -18,13 +28,24 @@ const loadLevelWord = (id) => {
 const wordDisplay = (words) => {
     const wordCont = document.getElementById("word-container")
     wordCont.innerHTML = "";
+    if (words.length == 0) {
+        wordCont.innerHTML = `
+         <div class="mx-auto text-center col-span-3 space-y-4">
+            <img class="mx-auto" src="./assets/alert-error.png" alt="">
+            <p class="font-bangla text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-bangla text-3xl text-[#292524] font-semibold">নেক্সট Lesson এ যান</h2>
+        </div>
+        `;
+        return;
+    }
+
     words.forEach(word => {
         const wordDiv = document.createElement('div')
         wordDiv.innerHTML = `
         <div class="bg-white px-8 py-10 text-center rounded-xl space-y-3">
-            <h3 class="text-2xl text-[#000000] font-bold">${word.word}</h3>
-            <p class="text-xl text-[#000000] ">Meaning/Pronounciatio</p>
-            <h3 class="font-bangla text-[#383838] text-2xl font-semibold mb-15">"${word.meaning} / ${word.pronunciation}"</h3>
+            <h3 class="text-2xl text-[#000000] font-bold">${word.word ? word.word : "Word not find"}</h3>
+            <p class="text-xl text-[#000000] ">Meaning/Pronounciation</p>
+            <h3 class="font-bangla text-[#383838] text-2xl font-semibold mb-15">"${word.meaning ? word.meaning : "Meaning not find"} / ${word.pronunciation ? word.pronunciation : "Pronunciation not find"}"</h3>
             <div class="flex justify-between">
                 <button class="btn bg-[#1A91FF30]"><i class="fa-solid text-[#000000] fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF30]"><i class="fa-solid text-[#000000] fa-volume-high"></i></button>
@@ -45,7 +66,7 @@ const displayLesson = (lessons) => {
     for (let lesson of lessons) {
         const btnDiv = document.createElement('div')
         btnDiv.innerHTML = `
-        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">Lesson ${lesson.level_no} </button>
+        <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="lesson-btn btn btn-outline btn-primary">Lesson ${lesson.level_no} </button>
         `
         levelCont.appendChild(btnDiv)
     }
